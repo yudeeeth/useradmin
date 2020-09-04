d740a21e978d67a58ae1969af1c0d136786f60b2
yudeeeth <udithkumarvd@gmail.com>
2020-09-04 12:17:04 +0530

    aliases for nearest and finalattendance

--- aliases.sh
+++ aliases.sh
@@ -8,5 +8,5 @@ alias permit="bash /root/userad/setpermisssions.sh"
 alias autoSchedule="bash /root/userad/assignpos.sh"
 alias attendance="bash /root/userad/setattendance.sh"
 alias record="bash $HOME/fetch.sh"
-
-
+alias nearest="bash /root/userad/nearest.sh"
+alias finalattendance="bash /root/userad/dailyattendance.sh"
